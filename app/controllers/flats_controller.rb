7d5b7b1156c0ef7a2fbf9c35aaa7fe901768cbcb
Boris Knezevic <boris.knezevic0@gmail.com>
2022-11-14 17:57:55 +0100

    show + test

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -5,4 +5,12 @@ class FlatsController < ApplicationController
     url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
     @flats = JSON.parse(URI.open(url).read)
   end
+
+  def show
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    flats = JSON.parse(URI.open(url).read)
+
+    id = params[:id]
+    @flat = flats.find { |flat| flat["id"] == id.to_i }
+  end
 end
